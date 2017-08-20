9620886a8e3f3615d8823f1e220e83507934f169
star16m <star16m@gmail.com>
2017-08-20 16:10:51 +0900

    change table ui.

--- src/main/java/star16m/utils/toapp/torrent/collector/TorrentCollector.java
+++ src/main/java/star16m/utils/toapp/torrent/collector/TorrentCollector.java
@@ -2,7 +2,6 @@ package star16m.utils.toapp.torrent.collector;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
