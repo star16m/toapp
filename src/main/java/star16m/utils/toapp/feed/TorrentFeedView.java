a011e6bd5a454a53afe248c844e517daaf61a2fd
star16m <star16m@gmail.com>
2020-05-30 15:09:53 +0900

    feet 다운 코드 일부 원복 및 uri encode 하도록 수정.

--- src/main/java/star16m/utils/toapp/feed/TorrentFeedView.java
+++ src/main/java/star16m/utils/toapp/feed/TorrentFeedView.java
@@ -1,5 +1,6 @@
 package star16m.utils.toapp.feed;
 
+import java.nio.charset.Charset;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
@@ -20,6 +21,7 @@ import com.rometools.rome.feed.rss.Item;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.util.UriUtils;
 import star16m.utils.toapp.torrent.Torrent;
 import star16m.utils.toapp.torrent.TorrentRepository;
 
@@ -54,7 +56,9 @@ public class TorrentFeedView extends AbstractRssFeedView {
 	private Item createItem(Torrent torrent) {
 		Item item = new Item();
 		item.setGuid(createGuid(torrent));
-		item.setLink("magnet:?xt=urn:btih:" + torrent.getMagnetCode());
+		String title = UriUtils.encode(torrent.getTitle().trim(), Charset.forName("UTF-8"));
+		item.setLink("magnet:?xt=urn:btih:" + torrent.getMagnetCode().trim()+ "&dn=" + title
+				+ "&tr=udp://tracker.openbittorrent.com:80&tr=http://megapeer.org:6969/announce&tr=http://mgtracker.org:2710/announce&tr=http://tracker.files.fm:6969/announce&tr=http://tracker.flashtorrents.org:6969/announce&tr=http://tracker.mg64.net:6881/announce&tr=http://tracker.nwps.ws:6969/announce&tr=http://tracker.ohys.net/announce&tr=http://tracker.tfile.me/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=udp://coppersurfer.tk:6969/announce&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.coppersurfer.com:1337/announce");
 		item.setTitle(torrent.getTitle());
 		item.setDescription(createDescription(torrent));
 		item.setPubDate(Date.from(torrent.getDownloadDate().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()));
